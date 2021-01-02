d070bb516160fcc11cee52e2c8ad08f7ffca69af
SitaRamSai <57385355+SitaRamSai@users.noreply.github.com>
2021-01-02 17:23:20 -0600

    Added boilerplate code to redux

---
+++ components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+    componentDidMount(){
+        
+    }
+    render() {
+        return (
+        <View style={{flex:1, justifyContent:"center"}}>
+            <Text>User is Logged In</Text>
+          </View>
+        )
+    }
+}
+
+export default Main
